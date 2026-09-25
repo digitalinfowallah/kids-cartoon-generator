3b3b46aba5da16b752d2c341f29e07f557343217
digitalinfowallah <digitalinfowallah@gmail.com>
2026-09-25 13:29:17 +0530

    Add create_subtitles function for cartoon scenes

---
+++ app/subtitles.py
@@ -0,0 +1,16 @@
+def create_subtitles(scenes):
+    """
+    Create subtitles for the cartoon scenes.
+    """
+
+    subtitles = []
+
+    for scene in scenes:
+        subtitles.append({
+            "scene": scene["scene_number"],
+            "text": scene["dialogue"],
+            "start": 0,
+            "end": scene["duration"]
+        })
+
+    return subtitles
